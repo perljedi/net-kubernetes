322fb159c0aed067496b017b1041c9d4f70c7560
Dave Mueller <dmueller@liquidweb.com>
2015-06-24 10:13:57 -0400

    Adding 'get_pods' feature to ReplicationController resource object

--- lib/Net/Kubernetes/Resource/ReplicationController.pm
+++ lib/Net/Kubernetes/Resource/ReplicationController.pm
@@ -1,6 +1,7 @@
 package Net::Kubernetes::Resource::ReplicationController;
 
 use Moose;
+use URI;
 
 =head1 NAME
 
@@ -11,5 +12,33 @@ Net::Kubernetes::Resource::ReplicationController
 extends 'Net::Kubernetes::Resource';
 with 'Net::Kubernetes::Resource::State';
 with 'Net::Kubernetes::Resource::Spec';
+with 'Net::Kubernetes::Role::ResourceFactory';
+
+sub get_pods {
+	my($self) = @_;
+	my $uri = URI->new_abs("../pods", $self->path);
+	$uri->query_form(labelSelector=>$self->_build_selector_from_hash($self->spec->{selector}));
+	my $res = $self->ua->request($self->create_request(GET => $uri));
+	if ($res->is_success) {
+		my $pod_list = $self->json->decode($res->content);
+		my(@pods)=();
+		foreach my $pod (@{ $pod_list->{items}}){
+			$pod->{apiVersion} = $pod_list->{apiVersion};
+			push @pods, $self->create_resource_object($pod, 'Pod');
+		}
+		return wantarray ? @pods : \@pods;
+	}else{
+		Net::Kubernetes::Exception->throw(code=>$res->code, message=>$res->message);
+	}
+}
+
+sub _build_selector_from_hash {
+	my($self, $select_hash) = @_;
+	my(@selectors);
+	foreach my $label (keys %{ $select_hash }){
+		push @selectors, $label.'='.$select_hash->{$label};
+	}
+	return \@selectors;
+}
 
 return 42;
