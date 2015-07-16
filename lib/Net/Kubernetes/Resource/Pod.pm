c2cbb99c1c45a49b5edbffd112c9fdb649afbf60
Dave Mueller <dmueller@liquidweb.com>
2015-07-16 09:41:34 -0400

    Adding logs to Change pod->logs, adding documentation for pod->logs

--- lib/Net/Kubernetes/Resource/Pod.pm
+++ lib/Net/Kubernetes/Resource/Pod.pm
@@ -8,6 +8,13 @@ extends 'Net::Kubernetes::Resource';
 with 'Net::Kubernetes::Resource::Role::State';
 with 'Net::Kubernetes::Resource::Role::Spec';
 
+=method logs([container=>'foo'])
+
+This method will return the logs from STDERR on for containers in a pod.  If the pod has more than one container,
+the container argument become manditory, however for single container pods it may be ommited.
+
+=cut
+
 sub logs {
 	my($self, %options) = @_;
 	if (scalar(@{ $self->spec->{containers} }) > 1 && ! exists($options{container})) {
